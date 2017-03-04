3d26055dd36aed10f8bbe4d15271db01b8f280de
Olivier GUIMONNEAU <oguimonneau@gmail.com>
2017-03-04 11:52:27 +0100

    Accès à la database + création du formulaire

--- tp1_mini_chat/index.php
+++ tp1_mini_chat/index.php
@@ -1,2 +1,50 @@
-<?php 
+<?php
+//Connection à la databe
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'root');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon premier mini-Chat</title>
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    
+    <header>
+       
+    </header>
+    
+   
+    <!-- Le formulaire -->
+    
+  	<center>
+  			<form action="cible.php" method="post">
+			<p>
+			    Votre pseudo : <input type="text" name="pseudo" /><br/>
+			    Votre message : <input type="text" name="message" /><br/>
+			    <input type="submit" value="Envoyer" />
+			</p>
+		</form>
+  	</center>
+		    
+    <!-- La liste des massages -->
+
+    <!-- Le pied de page -->
+
+    <footer id="pied_de_page">
+        <p>Copyright Kahilom, tous droits réservés</p>
+    </footer>
+    
+    </body>
+</html>
