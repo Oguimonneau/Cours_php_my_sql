daa8734f6c7644bb6fc1d0374c37a01c663246e0
Olivier GUIMONNEAU <oguimonneau@gmail.com>
2017-03-04 12:30:56 +0100

    Mise en forme de la liste des message + classement inverse

--- tp1_mini_chat/index.php
+++ tp1_mini_chat/index.php
@@ -40,17 +40,17 @@ catch (Exception $e)
   	</center>
 		    
     <!-- La liste des messages -->
-<?php
-$reponse = $bdd->query('SELECT * FROM messages_chat');
-   // On affiche chaque entrée une à une
-while ($donnees = $reponse->fetch())
-{
-    echo $donnees['pseudo']. ' : '.$donnees['message'];
-}
-$reponse->closeCursor(); // Termine le traitement de la requête
-
-
-?>
+    <?php
+    $reponse = $bdd->query('SELECT * FROM messages_chat order by id_chat desc');
+       // On affiche chaque entrée une à une
+    while ($donnees = $reponse->fetch())
+    {
+    ?>
+        <p><strong><?php echo $donnees['pseudo']?></strong> : <?php echo $donnees['message']?></p> 
+        <?php 
+    }
+    $reponse->closeCursor(); // Termine le traitement de la requête
+    ?>
     <!-- Le pied de page -->
 
     <footer id="pied_de_page">
