d9e4baf417bf27f3038d4982853703a54ac0bfff
Olivier GUIMONNEAU <oguimonneau@gmail.com>
2017-03-05 11:58:26 +0100

    Bootstrap et JQuery CDN

--- tp1_mini_chat/index.php
+++ tp1_mini_chat/index.php
@@ -15,12 +15,11 @@ catch (Exception $e)
 <html>
     <head>
         <meta charset="utf-8" />
-        <!-- Utilise Bootstrap -->
-        <!--  Production <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
-        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-        <script src="bootstrap/js/jquery.js"></script>
-        <!--  Production <script src="bootstrap/js/bootstrap.min.js"></script>-->
-        <script src="bootstrap/js/bootstrap.js"></script>
+
+         <!-- Utilise Bootstrap CDN -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
         <title>Mon premier mini-Chat</title>
     </head>
